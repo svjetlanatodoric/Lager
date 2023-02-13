2026dcf8d45d978c455ee1017ecb89083a77bf94
Svjetlana <svjetlanatodoric03@gmail.com>
2023-02-13 18:10:40 +0100

    Verifying admin updated

--- register/if-admin-selected.php
+++ register/if-admin-selected.php
@@ -1,25 +1,31 @@
 <?php
 
 if ($rola == 1) {
-    header("Location: modal.php");
-    
-    // $sql_query = "
-    // INSERT INTO `korisnik`
-    // (`KorisnickoIme`,
-    // `Sifra`,
-    // `RolaId`)
-    // VALUES
-    // ('$korisnickoIme',
-    // '$password',
-    // '$rola')";
+    if (!empty($response) && $response->num_rows >= 1) {
+        header("Location: ../account-already-exists.php");
+    } else {
+        header("Location: modal.php");
 
-    //  setcookie('korisnickoIme', htmlspecialchars($_POST['korisnickoIme']), time() + 3600, '/');
-    //  $_COOKIE['korisnickoIme'] = $korisnickoIme;
-    //  if (isset($_COOKIE['korisnickoIme'])) {
-    //      header("Location: ../pocetna-stranica.php");
-    //      $db->close();
-    //      die();
-    //  }
-    // $db->query($sql_query);
-} 
+    require_once("admin-verify.php");
+        setcookie('korisnickoIme', htmlspecialchars($_POST['korisnickoIme']), time() + 3600, '/');
+        $_COOKIE['korisnickoIme'] = $korisnickoIme;
+        if (isset($_COOKIE['korisnickoIme'])) {
+            //  header("Location: ../pocetna-stranica.php");
+            
+        $sql_query = "
+        INSERT INTO `korisnik`
+        (`KorisnickoIme`,
+        `Sifra`,
+        `RolaId`)
+        VALUES
+        ('$korisnickoIme',
+        '$password',
+        '$rola')";
+    
+            $db->close();
+            die();
+        }
+        $db->query($sql_query);
+    }
+}
 ?>
