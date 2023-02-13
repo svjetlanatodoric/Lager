d49bb319d144bc93430fb865d8c27ea222468f0f
Svjetlana <svjetlanatodoric03@gmail.com>
2023-02-13 18:53:43 +0100

    login request modified

--- login/login-request.php
+++ login/login-request.php
@@ -3,40 +3,11 @@
     <link rel="stylesheet" href="../css/login-form.css">
 </head>
 <?php
-// if (!isset($_POST['korisnickoIme']) || !isset($_POST['sifra'])) {
-//     header("Location: login.php");
-//     exit;
-// }
-
-// $korisnickoIme = htmlspecialchars($_POST['korisnickoIme']);
-// $sifra = htmlspecialchars($_POST['sifra']);
-
-// $connection = new mysqli("localhost", "root", "", "prodavnica_db");
-
-// if ($connection->connect_error) {
-//     die("Greška pri konekciji sa bazom podataka: " . $connection->connect_error);
-// }
-
-// $sql = "SELECT KorisnickoIme, Sifra FROM `korisnik` WHERE KorisnickoIme='$korisnickoIme'";
-// $result = $connection->query($sql);
-
-// if ($result->num_rows ==1 ) {
-//     $row = $result->fetch_assoc();
-//     $hashed_password = $row['Sifra'];
-
-//     if (password_verify($sifra, $hashed_password)) {
-//         setcookie('korisnickoIme', $korisnickoIme, time() + 3600);
-//         header("Location: ../pocetna-stranica.php");
-//         exit;
-//     } else {
-//         require_once("login.php");
-//         echo "<p>Pogresna sifra</p>";
-//     }
-// } else {
-//     require_once("login.php");
-//     echo "<p>Ovaj korisnik ne postoji</p>";
-// }
-
+if (isset($_COOKIE['korisnickoIme'])) {
+    unset($_COOKIE['korisnickoIme']);
+    setcookie('korisnickoIme', null, -1, '/');
+    header("Location: ../login/login.php");
+}
 if (isset($_POST['korisnickoIme']) && isset($_POST['sifra'])) {
 
     include_once("../constants.php");
@@ -61,27 +32,14 @@ if (isset($_POST['korisnickoIme']) && isset($_POST['sifra'])) {
         }
         if (password_verify($password, $dbsifra) == false) {
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-                include_once("../constants.php");
-                $connection = mysqli_connect(SERVER_NAME, USERNAME, PASSWORD, DB_NAME);
-                $password = htmlspecialchars($_POST['sifra']);
-                $sql = "SELECT KorisnickoIme,  Sifra FROM `korisnik` ";
-                $result = $connection->query($sql);
-
-                $row = $result->fetch_assoc();
-                $dbsifra = $row['Sifra'];
-                if (password_verify($password, $dbsifra) == false) {
-                    require_once("login.php");
-                    require_once("forgot-password.php");
-                    require_once("password-recovery.php");
-                }
+                require_once("login.php");
             }
         }
-    } else {
-        require_once("login.php");
-        echo "<p>Ovaj korisnik ne postoji</p>";
-    };
-
+    } else{
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            require_once("login.php");
+        }
+    }
     if (isset($_POST['logout'])) {
         require_once("login.php");
     }
