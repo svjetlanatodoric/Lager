b9305a7dcde0bee9e78e6f1844d491de342fe47e
Svjetlana <svjetlanatodoric03@gmail.com>
2023-02-13 19:01:15 +0100

    Password recovery modal fixed

--- login/password-recovery-request.php
+++ login/password-recovery-request.php
@@ -1,3 +1,6 @@
+<head>
+    <link rel="stylesheet" href="../css/modal.css">
+</head>
 <?php
 include_once("../constants.php");
 $korisnickoIme = htmlspecialchars($_POST['korisnickoIme'], ENT_QUOTES, 'UTF-8');
@@ -12,54 +15,12 @@ if (isset($korisnickoIme)) {
     if ($passwordConfirm === $newPassword) {
         $sql_query = "UPDATE `korisnik` SET Sifra='" . password_hash($newPassword, PASSWORD_BCRYPT) . "' WHERE KorisnickoIme='$korisnickoIme'";
         $response = $connection->query($sql_query);
-        echo "<p>Sifra uspesno promenjena</p>";
-    } else {
-        require_once("login.php");
-?>
-<head>
-     <link rel="stylesheet" href="../css/login-form.css">
-     <link rel="stylesheet" href="../css/modal.css">
- </head>
-        <body>
-
-            <div id="modal" class="modal">
-
-                <!-- Sadržaj modala -->
-                <div class="entire-modal-content">
-                    <span id="close" onclick="modalExit()">&times;</span>
-                    <div class="modal-content">
-                        <form id="recovery-form" action="password-recovery-request.php" method="post">
-                            <h3>Promijenite lozinku</h3>
-                            <div>
-                                <labe>Unesite svoje korisnicko ime:</label> <br>
-                                    <input type="text" id="korisnickoIme" name="korisnickoIme" required><br>
-                            </div>
-                            <div>
-                                <label>Unesite novu lozinku: </label> <br>
-                                <input type="password" id="newPassword" name="newPassword" required><br>
-                            </div>
-                            <div>
-                                <label>Ponovo unesite novu lozinku: </label> <br>
-                                <input type="password" id="confirmPassword" name="passwordConfirm" required> <br>
-                            </div>
-                            <div>
-                                <p>Unesene lozinke moraju biti jednake</p>
-                            </div>
-                            <button type="submit">Potvrdi</button>
-                    </div>
-                    </form>
-                </div>
-
-            </div>
-            <script>
-               var modal =document.getElementById("modal")
-               modal.style.display="block"
-            </script>
-            <script src="../js/modal.js"></script>
-
-        </body>
-<?php
-        
+        header("Location: login.php");
+    } 
+    if ($passwordConfirm != $newPassword) {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        require_once("password-recovery.php");
+        }
     }
-}
-?>
+    }
+        ?>
