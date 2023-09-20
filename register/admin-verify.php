f2753848a1102aec66c5af4d82def6af8c33b1c3
Svjetlana <svjetlanatodoric03@gmail.com>
2023-09-20 10:43:25 +0200

    redirect to admin-a.php

--- register/admin-verify.php
+++ register/admin-verify.php
@@ -1,16 +1,16 @@
 <?php
 $idAdmin = htmlspecialchars($_POST['idAdmin'], ENT_QUOTES, 'UTF-8');
 if (isset($idAdmin) && $idAdmin == 1) {
-        header("Location: ../pocetna-stranica.php");
-        $db->close();
-        die();
-    
-} else{
+    header("Location: ../admin/artikli-a.php");
+
+    $db->close();
+    die();
+} else {
     require_once("modal.php");
-    ?>
+?>
     <script>
         alert("Unesite tačan identifikacioni broj!")
     </script>
-    <?php
+<?php
 }
 ?>
