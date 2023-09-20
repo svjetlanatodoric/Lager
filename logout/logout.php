abba5a83b62624191aa6149427113327ef68c2bd
Svjetlana <svjetlanatodoric03@gmail.com>
2023-09-20 10:59:08 +0200

    edited deleting cookies

--- logout/logout.php
+++ logout/logout.php
@@ -1,11 +1,15 @@
 <?php
-if (isset($_COOKIE['korisnickoIme'])) {
+if (isset($_COOKIE['korisnickoIme'])&&isset($_COOKIE['rola'])) {  
     unset($_COOKIE['korisnickoIme']); 
+    unset($_COOKIE['rola']); 
     setcookie('korisnickoIme', null, -1, '/'); 
+    setcookie('rola', null, -1, '/'); 
     header("Location: ../login/login.php");
     return true;
-} else {
+} 
+else {
+    header("Location: ../login/login.php");
+
     return false;
 }
-
 ?>
