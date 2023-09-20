ab628afc3abe3afc3f397b70e892e7e665ae5684
Svjetlana <svjetlanatodoric03@gmail.com>
2023-09-20 10:54:15 +0200

    deleting lager JS

---
+++ js/lager-delete.js
@@ -0,0 +1,10 @@
+var deleteLager = document.getElementById("form-delete-lager");
+var tabela = document.getElementById("tabela-container");
+function obrisiLager() {
+  deleteLager.style.display = "block";
+  tabela.style.transform = "translate(0,100%)";
+}
+
+//   function zatvoriDelModal(){
+//     deleteModal.style.display = "none";
+// }
