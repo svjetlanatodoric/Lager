3ac9ef4871ac7088df913199f537adf0c3364d5c
Svjetlana <svjetlanatodoric03@gmail.com>
2024-04-24 11:33:49 +0200

    Changed logic for editing data in table: now it's getting the ID of the item from DB and comparing it with ID in the hidden input field. Then it updates database based on that ID number.

--- admin/artikli-edit-request.php
+++ admin/artikli-edit-request.php
@@ -3,25 +3,26 @@ if (isset($_POST['sifra'])) {
     include_once("../constants.php");
     $conn = new mysqli(SERVER_NAME, USERNAME, PASSWORD, DB_NAME);
 
-  //  $sifraSelect = htmlspecialchars($_POST['sifraArtikla'], ENT_QUOTES, 'UTF-8');
+    //  $sifraSelect = htmlspecialchars($_POST['sifraArtikla'], ENT_QUOTES, 'UTF-8');
+    $artikalId = htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8');
     $sifra = htmlspecialchars($_POST['sifra'], ENT_QUOTES, 'UTF-8');
     $naziv = htmlspecialchars($_POST['naziv'], ENT_QUOTES, 'UTF-8');
     $mjera = htmlspecialchars($_POST['jedinicaMjere'], ENT_QUOTES, 'UTF-8');
     $barkod = htmlspecialchars($_POST['barkod'], ENT_QUOTES, 'UTF-8');
     $plu = htmlspecialchars($_POST['pluKod'], ENT_QUOTES, 'UTF-8');
 
-  try { 
-     $sql_query = "
+    try {
+        $sql_query = "
     SELECT
     *
 FROM
     artikal
 WHERE
-    Sifra = '$sifra' ";
-    $response = $conn->query($sql_query);
+    ArtikalId = '$artikalId' ";
+        $response = $conn->query($sql_query);
 
-    if (!empty($response) && $response->num_rows > 0) {
-        $sql_query = "
+        if (!empty($response) && $response->num_rows > 0) {
+            $sql_query = "
         UPDATE 
             `artikal`
         SET
@@ -30,18 +31,17 @@ WHERE
             `JedinicaMjere` =  '" . $mjera . "',
             `Barkod` = '" . $barkod . "',
             `PLU_KOD` = '" . $plu . "'
-        WHERE
-            `artikal`.`Sifra` = '$sifra'";
-        $conn->query($sql_query);
-    }
-  }  catch (mysqli_sql_exception $e) {
-            echo "MySQLi error: " . $e->getMessage();
-        } catch (Exception $e) {
-            // handle all other exceptions
-            echo "Error: " . $e->getMessage();
+        
+        WHERE `ArtikalId` = '$artikalId'";
+            $conn->query($sql_query);
         }
-    
-        $conn->close();
-        header("Location: artikli-a.php");
+    } catch (mysqli_sql_exception $e) {
+        echo "MySQLi error: " . $e->getMessage();
+    } catch (Exception $e) {
+        // handle all other exceptions
+        echo "Error: " . $e->getMessage();
     }
 
+    $conn->close();
+    header("Location: artikli-a.php");
+}
